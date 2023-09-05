e6151f2775f05405615670aa7568476b765bdd2e
SarahKimball <126026284+SarahKimball@users.noreply.github.com>
2023-09-05 06:29:27 -0400

    Add files via upload

---
+++ radix.js
@@ -0,0 +1,32 @@
+function getDigit(num, place) {
+    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+  }
+  
+  function digitCount(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+  }
+  
+  function mostDigits(nums) {
+    let maxDigits = 0;
+    for (let num of nums) {
+      maxDigits = Math.max(maxDigits, digitCount(num));
+    }
+    return maxDigits;
+  }
+  
+  function radixSort(nums) {
+    const maxDigitCount = mostDigits(nums);
+    for (let k = 0; k < maxDigitCount; k++) {
+      const digitBuckets = Array.from({ length: 10 }, () => []);
+      for (let num of nums) {
+        const digit = getDigit(num, k);
+        digitBuckets[digit].push(num);
+      }
+      nums = [].concat(...digitBuckets);
+    }
+    return nums;
+  }
+  
+  module.exports = radixSort;
+  
